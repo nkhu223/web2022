1cb9e2941ea85a2277722753849b85d38a8fa30d
Natalie <natthutch011001@gmail.com>
2022-04-21 15:52:40 -0400

    got rid of kirby on the about us page and changed dark mode time

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,27 @@
-if (new Date().getHours() > 14) {
+if (new Date().getHours() > 18) {
   const html = document.querySelector('html');
   html.style.backgroundColor = '#000';
   html.style.color = 'white';
 }
 
-if (new Date().getHours() > 14) {
+if (new Date().getHours() > 18) {
   const header = document.querySelector('header');
   header.style.backgroundColor = '#000';
   header.style.color = 'white';
 }
 
-if (new Date().getHours() > 14) {
+if (new Date().getHours() > 18) {
   const wrapperBody = document.getElementsByClassName('wrapper');
   wrapperBody[0].style.backgroundColor = '#000';
   wrapperBody[0].style.color = 'white';
 }
-if (new Date().getHours() > 14) {
+if (new Date().getHours() > 18) {
   const body = document.querySelector('body');
   body.style.backgroundColor = '#355082';
   body.style.color = 'white';
 }
 
-if (new Date().getHours() > 14) {
+if (new Date().getHours() > 18) {
   const footer = document.getElementsByClassName('footer');
   footer[0].style.backgroundColor = '#355082';
   footer[0].style.color = 'white';
