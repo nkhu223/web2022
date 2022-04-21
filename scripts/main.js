37fad62157f74f168f690f94bcbbca2f84ebbf60
Natalie <natthutch011001@gmail.com>
2022-04-21 15:15:03 -0400

    javascript dark mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,28 @@
 if (new Date().getHours() > 14) {
-  //alert(new Date().getHours());
   const html = document.querySelector('html');
-  html.style.backgroundColor = 'black';
+  html.style.backgroundColor = '#000';
   html.style.color = 'white';
 }
 
 if (new Date().getHours() > 14) {
-  const wrapperBody = document.getElementsByClassName('wrapper');
-  wrapperBody[0].style.backgroundColor = 'black';
-  wrapperBody[0].style.color = 'white';
-}
-if (new Date().getHours() > 14) {
-  const content = document.getElementsByClassName('contentbody');
-  content[0].style.backgroundColor = '#1B1212';
-  content[0].style.color = 'white';
-}
-
-if (new Date().getHours() > 14) {
-  const home = document.getElementsByClassName('homeimages');
-  home[0].style.backgroundColor = '#1B1212';
-  home[0].style.color = 'white';
-}
-
-if (new Date().getHours() > 14) {
-  const welcome = document.getElementsByClassName('welcomemessage');
-  welcome[0].style.backgroundColor = '#666666';
-  welcome[0].style.color = 'white';
-  welcome[0].style.border = '1em solid #1B1212';
-}
-
-if (new Date().getHours() > 14) {
-  const nav = document.getElementsByClassName('navbar');
-  nav[0].style.backgroundColor = '#343434';
-  nav[0].style.color = 'white';
-}
-
-if (new Date().getHours() > 14) {
   const header = document.querySelector('header');
-  header.style.backgroundColor = '#28282B';
+  header.style.backgroundColor = '#000';
   header.style.color = 'white';
 }
 
 if (new Date().getHours() > 14) {
-  const footer = document.querySelector('footer');
-  footer.style.backgroundColor = '#28282B';
-  footer.style.color = 'white';
+  const wrapperBody = document.getElementsByClassName('wrapper');
+  wrapperBody[0].style.backgroundColor = '#000';
+  wrapperBody[0].style.color = 'white';
+}
+if (new Date().getHours() > 14) {
+  const body = document.querySelector('body');
+  body.style.backgroundColor = '#355082';
+  body.style.color = 'white';
+}
+
+if (new Date().getHours() > 14) {
+  const footer = document.getElementsByClassName('footer');
+  footer[0].style.backgroundColor = '#355082';
+  footer[0].style.color = 'white';
 }
